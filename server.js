57e629d80c4f20764bec49427479385a90ba9e46
Himanshu <himanshurajput6654@gmail.com>
2023-04-02 00:33:45 +0530

    Adding cors

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors());
+app.use(cors({ origin: "http://localhost:3000/"}));
 app.use(router);
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 // app.use(cors());
 // app.use(bodyParser.json({ extended: true }));
